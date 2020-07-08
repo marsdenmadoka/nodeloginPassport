242cebb18174c230d88c62840c9fc4bb97e3aa04
marsdenmadoka <marsmadoka98@gmail.com>
2020-07-08 10:38:07 +0300

    for github

--- routes/users.js
+++ routes/users.js
@@ -14,11 +14,11 @@ var crypto = require('crypto');//no need to install crypto it is ready made in n
  const EmailPassword=process.env.EMAILPASSWORD
  const ConnectString=process.env.MONGODBSTRING
  //mongodb+srv://marsden:yourpassword@cluster0-ewzaf.mongodb.net/dbname?retryWrites=true&w=majority
-mongoose.connect("mongodb+srv://marsden:madoka98@cluster0-ewzaf.mongodb.net/Registerforms?retryWrites=true&w=majority",{useNewUrlParser: true,useCreateIndex:true,useUnifiedTopology: true}); 
+mongoose.connect(ConnectString,{useNewUrlParser: true,useCreateIndex:true,useUnifiedTopology: true}); 
 var db=mongoose.connection; 
 db.on('error', console.log.bind(console, "connection error")); 
 db.once('open', function(callback){ 
-    console.log("connection to db succeeded"); 
+    console.log("connection succeeded"); 
 }) 
 
 
@@ -120,8 +120,8 @@ passport.use(new LocalStrategy(function(username, password, done) {
         var transporter = nodemailer.createTransport({
         service: 'Gmail',
           auth: {
-            user:"marsmadoka98@gmail.com",
-            pass:"@MADoka98"
+            user: Emailaddress,
+            pass: EmailPassword
           }
         });
         var mailOptions = {
@@ -184,8 +184,8 @@ router.get("/reset/:token", function(req, res) {
         var transporter = nodemailer.createTransport({
           service: 'Gmail',
           auth: {
-            user:"marsmadoka98@gmail.com",
-            pass:"@MADoka98"
+            user: Emailaddress,
+            pass: EmailPassword
           }
         });
         var mailOptions = {
